b953aa0fbac3439d1f0bf450baa02eed684e8de1
Kadin <144294293+kwern731@users.noreply.github.com>
2023-10-14 07:00:23 -0600

    start game over

--- scripts/player.gd
+++ scripts/player.gd
@@ -26,7 +26,7 @@ func _physics_process(delta):
 func take_damage(damage):
 	hp -= damage
 	if hp <= 0:
-		get_tree().quit()
+		get_tree().change_scene_to_file("res://scenes/)
 
 func _on_area_entered(area):
 		if area.is_in_group("enemies"):
