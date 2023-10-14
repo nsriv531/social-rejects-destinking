814451e9bd0452abea678f9ebc73d129581e5b42
Kadin <144294293+kwern731@users.noreply.github.com>
2023-10-14 01:41:10 -0600

    menu back up and shooting works when errors are commented

---
+++ scripts/level_select.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+func _on_l_1_pressed():
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
+	
+func _on_l_2_pressed():
+	get_tree().change_scene_to_file("res://scenes/.tscn")
+
+func _on_l_3_pressed():
+	get_tree().change_scene_to_file("res://scenes/.tscn")
+	
+func _on_l_4_pressed():
+	get_tree().change_scene_to_file("res://scenes/.tscn")
+	
+func _on_l_5_pressed():
+	get_tree().change_scene_to_file("res://scenes/.tscn")
+	
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
