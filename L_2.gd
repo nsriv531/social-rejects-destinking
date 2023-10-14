1103f7b89fb892864630ae5303aa8b30d6c94fcb
Abdullah Abdullah <atron1792@gmail.com>
2023-10-14 09:18:27 -0600

    level 2 shit

---
+++ L_2.gd
@@ -0,0 +1,85 @@
+extends Node2D
+
+var crayon = preload("res://scenes/crayon.tscn")
+var rng = RandomNumberGenerator.new()
+
+
+@onready var timer = $Timer
+#@onready var timer_level = $Timer_level
+@onready var marker = $Marker2D_1
+@onready var cacti = preload("res://scenes/enemy_teen.tscn")
+@onready var cactii = preload("res://scenes/Book.tscn")
+@onready var cactiii = preload("res://scenes/math.tscn")
+
+func _ready():
+	$Timer.start()
+#	$Timer_level.start()
+	
+func _on_timer_timeout():
+	rng.randomize()
+	var number = rng.randi_range(1,10)
+	print(number)
+	if number == 1:
+		var cactus = cacti.instantiate()
+		cactus.position = $Marker2D_1.position
+		$enemy.add_child(cactus)
+	elif number == 2:
+		var cactus = cactii.instantiate()
+		cactus.position = $Marker2D_2.position
+		$enemy.add_child(cactus)
+	elif number == 3:
+		var cactus = cactii.instantiate()
+		cactus.position = $Marker2D_3.position
+		$enemy.add_child(cactus)
+	elif number == 4:
+		var cactus = cacti.instantiate()
+		cactus.position = $Marker2D_4.position
+		$enemy.add_child(cactus)
+	elif number == 5:
+		var cactus = cactii.instantiate()
+		cactus.position = $Marker2D_5.position
+		$enemy.add_child(cactus)
+	elif number == 6:
+		var cactus = cactiii.instantiate()
+		cactus.position = $Marker2D_6.position
+		$enemy.add_child(cactus)
+	elif number == 7:
+		var cactus = cacti.instantiate()
+		cactus.position = $Marker2D_7.position
+		$enemy.add_child(cactus)
+	elif number == 8:
+		var cactus = cactii.instantiate()
+		cactus.position = $Marker2D_8.position
+		$enemy.add_child(cactus)
+	elif number == 9:
+		var cactus = cactiii.instantiate()
+		cactus.position = $Marker2D_9.position
+		$enemy.add_child(cactus)
+	elif number == 10:
+		var cactus = cacti.instantiate()
+		cactus.position = $Marker2D_10.position
+		$enemy.add_child(cactus)
+	return "done"	
+
+var counter = 0
+
+func _e(location):
+	var c = crayon.instantiate()
+	c.global_position = location
+	add_child(c)
+
+func _on_player_teen_spawn_crayon(location):
+	var c = crayon.instantiate()
+	c.global_position = location
+	add_child(c)
+
+func _on_wall_right_body_entered(body):
+	if body.is_in_group("Player"):
+		global_position = Vector2(40, 861)
+		
+
+func _on_timer_level_timeout():
+	get_tree().change_scene_to_file("res://scenes/L_2.tscn")
+	
+
+
--- scripts/Player_teen.gd
+++ scripts/Player_teen.gd
@@ -9,11 +9,8 @@ var input_vector = Vector2.ZERO
 var hp = 3
 
 @onready var muzzle = $Muzzle
-@onready var kid = $Sprite2D/AnimationPlayer
-#@onready var health1 = $Health1
 
 func _physics_process(delta):
-	kid.play("kid_small")
 	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
@@ -28,15 +25,9 @@ func take_damage(damage):
 		get_tree().change_scene_to_file("res://scenes/")
 
 func _on_area_entered(area):
-		if area.is_in_group("enemies"):
-			area.take_damage(1)	
+	if area.is_in_group("enemies"):
+			area.take_damage(1)
 
 func shoot_crayon():
 	emit_signal("spawn_crayon", muzzle.global_position)
-
-
-	
-	
-	
-
 
